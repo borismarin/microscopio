c11b4e26a8fe978b2ddc695dc388a6efab4b996f
Edu Renesto <edure95@gmail.com>
2019-09-16 22:20:00 -0300

    C pipes PC: improving timing

--- learning/producer-consumer/pc-c-pipes/main.c
+++ learning/producer-consumer/pc-c-pipes/main.c
@@ -8,27 +8,43 @@
 #include "pipe.h"
 
 #define COUNT (2048*2048)
-#define BUFSIZE 1
+#define BUFSIZE 4
 
 typedef uint32_t Pixel; // Tipo do pixel, igual a impl. em rust
 
-void *producer_func(void* pProducer) {
-    pipe_producer_t *producer = (pipe_producer_t*) pProducer;
+typedef struct {
+    pipe_producer_t *pProducer;
+    char bSilent;
+    unsigned int nBufSize;
+} producer_thread_args_t;
+
+void *producer_func(void* pArgs) {
+    producer_thread_args_t *args = (producer_thread_args_t*) pArgs;
+    pipe_producer_t *producer = args->pProducer;
 
     Pixel data = 42;
 
-    clock_t begin = clock();
+    struct timespec begin;
+    memset(&begin, 0, sizeof(struct timespec));
+    struct timespec end;
+    memset(&end, 0, sizeof(struct timespec));
+
+    clock_gettime(CLOCK_REALTIME, &begin);
     for(int i = 0; i < COUNT; i++) {
         pipe_push(producer, &data, 1);
     }
-    clock_t end = clock();
+    clock_gettime(CLOCK_REALTIME, &end);
 
-    clock_t elapsed = begin - end;
-    int elapsed_ns = elapsed * 1000000000 / CLOCKS_PER_SEC;
+    double delta = (end.tv_sec - begin.tv_sec) + 
+                    (end.tv_nsec - begin.tv_nsec) / 1000000000;
 
-    float average = ((float) elapsed_ns)/COUNT;
+    double average = delta/COUNT;
 
-    printf("Average write: %g ns/px\n", average);
+    if(!args->bSilent) {
+        printf("Average write: %g ns/px\n", average * 1000000000);
+    } else {
+        printf("%g\n", average * 1000000000);
+    }
 
     pipe_producer_free(producer);
     
@@ -48,24 +64,34 @@ void *consumer_func(void* pConsumer) {
     return NULL;
 }
 
-void *producer_buffered_func(void* pProducer) {
-    pipe_producer_t *producer = (pipe_producer_t*) pProducer;
+void *producer_buffered_func(void* pArgs) {
+    producer_thread_args_t *args = (producer_thread_args_t*) pArgs;
+    pipe_producer_t *producer = args->pProducer;
+
+    struct timespec begin;
+    memset(&begin, 0, sizeof(struct timespec));
+    struct timespec end;
+    memset(&end, 0, sizeof(struct timespec));
 
     Pixel *data = malloc(sizeof(Pixel) * BUFSIZE);
     memset(data, 42, sizeof(Pixel) * BUFSIZE);
 
-    clock_t begin = clock();
-    for(int i = 0; i < COUNT / BUFSIZE; i++) {
-        pipe_push(producer, data, BUFSIZE);
+    clock_gettime(CLOCK_REALTIME, &begin);
+    for(int i = 0; i < COUNT / args->nBufSize; i++) {
+        pipe_push(producer, data, args->nBufSize);
     }
-    clock_t end = clock();
+    clock_gettime(CLOCK_REALTIME, &end);
 
-    clock_t elapsed = begin - end;
-    int elapsed_ns = elapsed * 1000000000 / CLOCKS_PER_SEC;
+    double delta = (end.tv_sec - begin.tv_sec) + 
+                    (end.tv_nsec - begin.tv_nsec) / 1000000000;
 
-    float average = ((float) elapsed_ns)/COUNT;
+    double average = delta/COUNT;
 
-    printf("Average write: %g ns/px\n", average);
+    if(!args->bSilent) {
+        printf("Average write: %g ns/px\n", average * 1000000000);
+    } else {
+        printf("%g\n", average * 1000000000);
+    }
 
     pipe_producer_free(producer);
     
@@ -86,7 +112,27 @@ void *consumer_buffered_func(void* pConsumer) {
 }
 
 int main(int argc, char* argv[]) {
-    printf("Running single pixel per send...\n");
+    char bSilent = 0;
+    unsigned int nBufSize = BUFSIZE;
+
+    if(argc < 2) {
+        printf("Usage: %s [nBufSize] [silent]\n", argv[0]);
+        printf("\twhere\n");
+        printf("\t\tnBufSize: the size of the buffer in pixels\n");
+        printf("\t\tsilent: either 0 (verbose) or 1 (silent)\n");
+        printf("\n");
+        printf("Assuming a buffer size of %d\n", BUFSIZE);
+    }
+
+    if(argc >= 2) {
+        nBufSize = atoi(argv[1]);
+    }
+
+    if(argc >= 3) {
+        bSilent = (char) atoi(argv[2]);
+    }
+
+    if(!bSilent) printf("Running single pixel per send...\n");
     pthread_t producer_thread;
     pthread_t consumer_thread;
 
@@ -97,13 +143,18 @@ int main(int argc, char* argv[]) {
 
     pipe_free(pipe);
 
-    pthread_create(&producer_thread, NULL, &producer_func, producer);
+    producer_thread_args_t args;
+    args.pProducer = producer;
+    args.nBufSize = nBufSize;
+    args.bSilent = bSilent;
+
+    pthread_create(&producer_thread, NULL, &producer_func, &args);
     pthread_create(&consumer_thread, NULL, &consumer_func, consumer);
 
     pthread_join(producer_thread, NULL);
     pthread_join(consumer_thread, NULL);
 
-    printf("Running buffered...\n");
+    if(!bSilent) printf("Running buffered with chunks of %d pixels...\n", nBufSize);
     pthread_t producer_buffered_thread;
     pthread_t consumer_buffered_thread;
 
@@ -114,7 +165,9 @@ int main(int argc, char* argv[]) {
 
     pipe_free(pipe_buffered);
 
-    pthread_create(&producer_buffered_thread, NULL, &producer_buffered_func, producer_buffered);
+    args.pProducer = producer_buffered;
+
+    pthread_create(&producer_buffered_thread, NULL, &producer_buffered_func, &args);
     pthread_create(&consumer_buffered_thread, NULL, &consumer_buffered_func, consumer_buffered);
 
     pthread_join(producer_buffered_thread, NULL);
