28c79c867d2fa93f6128c38696be26e70f386bee
Edu Renesto <edure95@gmail.com>
2020-03-23 21:51:59 -0300

    FPGA: Change random generation method

--- aoldaq/src/fpga.c
+++ aoldaq/src/fpga.c
@@ -63,6 +63,6 @@ uint32_t fpga_read(fpga_t *p_session, uint32_t *buf, uint32_t n) {
 // Fill buf with random data
 static void fpga_gen_random(uint32_t *buf, uint32_t n) {
     for(uint32_t i = 0; i < n; i++) {
-        buf[i] = rand() % (UINT32_MAX - 1);
+        buf[i] = rand() * UINT32_MAX / RAND_MAX;
     }
 }
