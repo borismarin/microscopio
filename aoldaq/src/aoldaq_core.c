c8229fba3caaefd4764198ed7531bca32e096403
Edu Renesto <edure95@gmail.com>
2020-04-20 22:14:33 -0300

    AOLDAQ: Add support for multiple channels

--- aoldaq/src/aoldaq_core.c
+++ aoldaq/src/aoldaq_core.c
@@ -8,52 +8,91 @@
 aoldaq_t *aoldaq_create_instance(aoldaq_args_t *p_args) {
     aoldaq_t *p_state = malloc(sizeof(aoldaq_t));
 
-    p_state->p_data_tx = os_pipe_alloc_producer();
-    p_state->p_data_rx = os_pipe_alloc_consumer();
+    // Allocate data for our FIFOs
+    p_state->p_data_txs = malloc(sizeof(os_pipe_producer_t*) * p_args->n_channels);
+    p_state->p_data_rxs = malloc(sizeof(os_pipe_consumer_t*) * p_args->n_channels);
 
-    os_pipe_create(p_state->p_data_rx, p_state->p_data_tx);
+    p_state->p_fpgas = malloc(sizeof(fpga_t*) * p_args->n_channels);
 
-    fpga_args_t args;
+    p_state->daq_threads = malloc(sizeof(pthread_t) * p_args->n_channels);
+    p_state->p_thread_args = malloc(sizeof(thread_args) * p_args->n_channels);
 
-    if(p_args->mode == AOLDAQ_MODE_RANDOM) {
-        args.mode = FPGA_MODE_RANDOM;
-    } else if(p_args->mode == AOLDAQ_MODE_BITMAP) {
-        args.mode = FPGA_MODE_BITMAP;
-        args.bitmap_data = p_args->bitmap_data;
-        args.bitmap_width = p_args->bitmap_width;
-        args.bitmap_height = p_args->bitmap_height;
-    } else if(p_args->mode == AOLDAQ_MODE_REAL) {
-        args.mode = FPGA_MODE_RANDOM;
-    }
+    p_state->n_channels = p_args->n_channels;
 
-    fpga_t *p_fpga = fpga_init_session(&args);
-
-    if(!p_fpga) {
-        // Bailing out, something happened
-        os_pipe_destroy(p_state->p_data_rx, p_state->p_data_tx);
-
-        os_pipe_free_consumer(p_state->p_data_rx);
-        os_pipe_free_producer(p_state->p_data_tx);
-
-        free(p_state);
-
-        return NULL;
-    }
-
-    p_state->p_fpga = p_fpga;
-    p_state->scan_params = p_args->scan_params;
     p_state->block_size = p_args->block_size;
+    p_state->scan_params = p_args->scan_params;
 
     p_state->running = 0;
     p_state->quit = 0;
 
-    if(pthread_create(&p_state->daq_thread, NULL, &daq_thread_fun, p_state) != 0) {
+    char success = 1;
+    char destroy_threads = 0;
+
+    uint8_t i;
+    for(i = 0; i < p_args->n_channels && success; i++) {
+        p_state->p_data_txs[i] = os_pipe_alloc_producer();
+        p_state->p_data_rxs[i] = os_pipe_alloc_consumer();
+
+        os_pipe_create(p_state->p_data_rxs[i], p_state->p_data_txs[i]);
+
+        fpga_args_t args;
+
+        if(p_args->mode == AOLDAQ_MODE_RANDOM) {
+            args.mode = FPGA_MODE_RANDOM;
+        } else if(p_args->mode == AOLDAQ_MODE_BITMAP) {
+            args.mode = FPGA_MODE_BITMAP;
+            args.bitmap_data = p_args->bitmap_data;
+            args.bitmap_width = p_args->bitmap_width;
+            args.bitmap_height = p_args->bitmap_height;
+        } else if(p_args->mode == AOLDAQ_MODE_REAL) {
+            args.mode = FPGA_MODE_RANDOM;
+        }
+
+        fpga_t *p_fpga = fpga_init_session(&args);
+
+        if(!p_fpga) {
+            success = 0;
+            break;
+        }
+
+        p_state->p_fpgas[i] = p_fpga;
+
+        thread_args targs = {
+            .channel_idx = i,
+            .p_state = p_state,
+        };
+
+        p_state->p_thread_args[i] = targs;
+
+        if(pthread_create(p_state->daq_threads + i, NULL, &daq_thread_fun, p_state->p_thread_args + i) != 0) {
+            success = 0;
+            destroy_threads = 1;
+            break;
+        }
+    }
+
+    if(!success) {
         // Bailing out, something happened
-        os_pipe_destroy(p_state->p_data_rx, p_state->p_data_tx);
+        if(destroy_threads) 
+            p_state->quit = 1;
 
-        os_pipe_free_consumer(p_state->p_data_rx);
-        os_pipe_free_producer(p_state->p_data_tx);
+        for(uint8_t j = 0; j < i; j++) {
+            if(destroy_threads) {
+                pthread_cancel(p_state->daq_threads[i]);
+                pthread_join(p_state->daq_threads[i], NULL);
+            }
 
+            os_pipe_destroy(p_state->p_data_rxs[j], p_state->p_data_txs[j]);
+
+            os_pipe_free_consumer(p_state->p_data_rxs[j]);
+            os_pipe_free_producer(p_state->p_data_txs[j]);
+        }
+
+        free(p_state->p_data_txs);
+        free(p_state->p_data_rxs);
+        free(p_state->p_fpgas);
+        free(p_state->daq_threads);
+        free(p_state->p_thread_args);
         free(p_state);
 
         return NULL;
@@ -68,15 +107,22 @@ void aoldaq_destroy_instance(aoldaq_t *p_state) {
     aoldaq_stop(p_state);
 
     p_state->quit = 1;
-    pthread_cancel(p_state->daq_thread);
-    pthread_join(p_state->daq_thread, NULL);
 
-    os_pipe_destroy(p_state->p_data_rx, p_state->p_data_tx);
+    for(uint8_t i = 0; i < p_state->n_channels; i++) {
+        pthread_cancel(p_state->daq_threads[i]);
+        pthread_join(p_state->daq_threads[i], NULL);
 
-    os_pipe_free_consumer(p_state->p_data_rx);
-    os_pipe_free_producer(p_state->p_data_tx);
+        os_pipe_destroy(p_state->p_data_rxs[i], p_state->p_data_txs[i]);
 
+        os_pipe_free_consumer(p_state->p_data_rxs[i]);
+        os_pipe_free_producer(p_state->p_data_txs[i]);
+    }
 
+    free(p_state->p_data_txs);
+    free(p_state->p_data_rxs);
+    free(p_state->p_fpgas);
+    free(p_state->daq_threads);
+    free(p_state->p_thread_args);
     free(p_state);
 }
 
@@ -89,16 +135,24 @@ void aoldaq_stop(aoldaq_t *p_state) {
 }
 
 void *daq_thread_fun(void *p_args_raw) {
-    aoldaq_t *p_args = (aoldaq_t*) p_args_raw;
+    thread_args *p_args = (thread_args*) p_args_raw;
 
-    uint32_t *back_buffer = (uint32_t *) malloc(sizeof(uint32_t) * p_args->block_size);
+    uint32_t *back_buffer = (uint32_t *) malloc(sizeof(uint32_t) * p_args->p_state->block_size);
 
-    while(!p_args->quit) {
-        if(!p_args->running) continue;
+    while(!p_args->p_state->quit) {
+        if(!p_args->p_state->running) continue;
         
-        uint32_t read = fpga_read(p_args->p_fpga, back_buffer, p_args->block_size);
+        uint32_t read = fpga_read(
+                p_args->p_state->p_fpgas[p_args->channel_idx], 
+                back_buffer,
+                p_args->p_state->block_size
+        );
 
-        os_pipe_write(p_args->p_data_tx, back_buffer, read * sizeof(uint32_t));
+        os_pipe_write(
+                p_args->p_state->p_data_txs[p_args->channel_idx],
+                back_buffer,
+                read * sizeof(uint32_t)
+        );
     }
 
     free(back_buffer);
@@ -107,6 +161,7 @@ void *daq_thread_fun(void *p_args_raw) {
 }
 
 uint32_t aoldaq_get_ramps(aoldaq_t *p_state, uint32_t n_cycles, ramp_t *buf) {
+#if 0
     switch(p_state->scan_params.mode) {
         case AOLDAQ_IMAGING_MODE_RASTER:
             // TODO improve
@@ -130,14 +185,19 @@ uint32_t aoldaq_get_ramps(aoldaq_t *p_state, uint32_t n_cycles, ramp_t *buf) {
             return 0;
             break;
     }
+#endif
+
+    return 0;
 }
 
 uint32_t aoldaq_get_voxels(aoldaq_t *p_state, uint8_t channel, uint32_t *buf, uint32_t n_voxels) {
     if(!p_state) {
         return 0;
     }
-
-    // TODO add support for multiple channels
     
-    return os_pipe_read(p_state->p_data_rx, buf, n_voxels * sizeof(uint32_t));
+    return os_pipe_read(
+            p_state->p_data_rxs[channel],
+            buf,
+            n_voxels * sizeof(uint32_t)
+     );
 }
